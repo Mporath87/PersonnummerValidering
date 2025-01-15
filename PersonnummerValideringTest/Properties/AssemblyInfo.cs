04ac1a1daea5b636c7fda774b913db9b42c4563c
Martin Porath <martin.porath@learnet.se>
2025-01-15 15:29:59 +0100

    Lade till projekt: PersonnummerValideringTest för enhetstestning. Har inte installerat nuggets

---
+++ PersonnummerValideringTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PersonnummerValideringTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PersonnummerValideringTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("533f4816-86ed-4c36-b0c6-54dcefca6c03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
